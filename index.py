3637f6642557e5b75221ec52a7789d1a0bb4040f
ankoz2000 <52471156+ankoz2000@users.noreply.github.com>
2021-02-24 14:27:43 +0300

    finish rating, new messages and fix problem with popitem()

--- index.py
+++ index.py
@@ -51,4 +51,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        print("EXCEPTION: ", e)
+    finally:
+        print("RESTARTING")
+        main()
